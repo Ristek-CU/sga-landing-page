db5560bb0455f69477bd58e602a9058b3c7dff59
RugeFX <zackfxg@gmail.com>
2025-01-13 22:36:18 +0700

    feat(fonts): configured fonts using fontsource variable lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import "@fontsource-variable/plus-jakarta-sans/wght.css";
+import "@fontsource-variable/plus-jakarta-sans/wght-italic.css";
 import "./index.css";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
