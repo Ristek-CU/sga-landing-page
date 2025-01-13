192447142f991ef396fa06775567776773112439
RugeFX <zackfxg@gmail.com>
2025-01-13 22:11:35 +0700

    added color palette into tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<main className="flex flex-col items-center justify-center w-full min-h-screen gap-4 text-white bg-blue-800 ">
+		<main className="flex flex-col items-center justify-center w-full min-h-screen gap-4 text-white bg-blue-800">
 			<h1 className="text-2xl font-bold">SGA LANDING PAGE</h1>
 			<img src={sgaLogo} alt="SGA Logo" className="size-52" />
 			<div className="flex flex-col items-center justify-center space-y-4 text-xl">
 				<p>A COUNTER FOR SOME REASON: {count}</p>
 				<button
-					className="p-2 border border-white bg-emerald-900"
+					className="p-2 border border-white bg-green-900"
 					onClick={() => setCount((prev) => prev + 1)}
 				>
 					Add
