20cc2107922f0da66391dbadcb26347c977b8efa
RugeFX <zackfxg@gmail.com>
2025-01-14 23:37:54 +0700

    feat(mission): added mission section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import HeroSection from "./components/sections/hero";
+import MissionSection from "./components/sections/mission";
 
 function App() {
 	return (
 		<main className="grid w-full min-h-screen">
 			{/* Hero Section */}
 			<HeroSection />
+
+			{/* Mission Section */}
+			<MissionSection />
 		</main>
 	);
 }
