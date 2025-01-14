a614444df341fa733321db2e44526ef9003d74cf
RugeFX <zackfxg@gmail.com>
2025-01-14 22:01:01 +0700

    chore(hero): revert initial test changes

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
 				<div className="group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-white/10 px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f]">
 					<div className="absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-blue-300/50 via-green-300/100 to-yellow-300/100 bg-[length:var(--bg-size)_100%] p-[1px] ![mask-composite:subtract] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]" />
 					<span className="inline-block text-lg font-semibold text-center text-transparent text-max bg-gradient-to-r from-blue-300 via-green-300 to-yellow-300 bg-clip-text">
-						Welcome To
+						Welcome to
 					</span>
 				</div>
 				<h1 className="grid text-6xl font-semibold leading-snug text-center">
