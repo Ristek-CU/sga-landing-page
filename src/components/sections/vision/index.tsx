504de935f2a2957e34358cf8a5c39f3ad3539a65
RugeFX <zackfxg@gmail.com>
2025-01-16 14:09:05 +0700

    fix(vision): minor styling error fix

--- src/components/sections/vision/index.tsx
+++ src/components/sections/vision/index.tsx
@@ -1,17 +1,24 @@
 import { Badge } from "@/components/ui/badge";
-import photoSga from "@/assets/images/photoSga.png";
+import sgaMembers from "@/assets/images/sga-members.png";
 
 export default function VisionSection() {
-    return (
-        <section className='flex flex-col w-full h-full max-w-screen-xl gap-10 px-10 py-32 mx-auto bg-white lg:flex-row'>
-            <img src={photoSga} />
-            <div className='flex-1 space-y-6'>
-                <Badge className='mb-2'>Vision</Badge>
-                <h1 className='text-4xl font-semibold'>Inspirasi Kepemimpinan dan Inovasi di
-                    <span className='text-green-500'>Lingkungan Kampus</span>
-                </h1>
-                <p className='text-gray-300'>Menjadi pelopor dalam pengembangan kepemimpinan mahasiswa yang berkelanjutan, dengan menciptakan lingkungan yang mendorong inovasi, kolaborasi, dan inklusivitas. Kami berkomitmen untuk memberdayakan setiap mahasiswa agar dapat menjadi pemimpin yang berintegritas, visioner, dan berkontribusi positif.</p>
-            </div>
-        </section>
-    )
+	return (
+		<section className="flex flex-col w-full h-full max-w-screen-xl gap-10 px-10 py-32 mx-auto bg-white lg:flex-row">
+			<img src={sgaMembers} className="flex-1 w-full h-max" />
+			<div className="flex-1 space-y-6">
+				<Badge>Vision</Badge>
+				<h1 className="text-4xl font-semibold leading-tight">
+					Inspirasi Kepemimpinan dan Inovasi di
+					<span className="text-green-500">Lingkungan Kampus</span>
+				</h1>
+				<p className="text-xl font-normal text-gray-300">
+					Menjadi pelopor dalam pengembangan kepemimpinan mahasiswa yang
+					berkelanjutan, dengan menciptakan lingkungan yang mendorong inovasi,
+					kolaborasi, dan inklusivitas. Kami berkomitmen untuk memberdayakan
+					setiap mahasiswa agar dapat menjadi pemimpin yang berintegritas,
+					visioner, dan berkontribusi positif.
+				</p>
+			</div>
+		</section>
+	);
 }
