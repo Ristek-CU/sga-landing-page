b1581f7266ec91a3c33fedff9bdbc7319093337f
RugeFX <zackfxg@gmail.com>
2025-01-16 13:24:12 +0700

    chore(about): fixed naming convention

--- src/components/sections/about-us/index.tsx
+++ src/components/sections/about-us/index.tsx
@@ -1,17 +1,24 @@
-import { Badge } from '@/components/ui/badge'
-import photoSga from "@/assets/images/photoSga.png";
+import sgaMembers from "@/assets/images/sga-members.png";
+import { Badge } from "@/components/ui/badge";
 
 export default function AboutUsSection() {
-    return (
-        <section className='flex flex-col w-full h-full max-w-screen-xl gap-10 px-10 py-32 mx-auto bg-white lg:flex-row'>
-            <div className='flex-1 space-y-6'>
-                <Badge className='mb-2'>About Us</Badge>
-                <h1 className='text-4xl font-semibold'>Kenali Lebih Dekat SGA{" "}
-                    <span className='text-blue-500'>Cakrawala University</span>
-                </h1>
-                <p className='text-gray-300'>Student Government Association (SGA) Cakrawala University adalah wadah resmi bagi mahasiswa untuk berpartisipasi dalam pengambilan keputusan yang mempengaruhi kehidupan kampus. Kami berkomitmen untuk menjadi jembatan antara mahasiswa dan pihak universitas, serta mempromosikan kepemimpinan yang inklusif.</p>
-            </div>
-            <img src={photoSga} />
-        </section>
-    )
+	return (
+		<section className="flex flex-col w-full h-full max-w-screen-xl gap-10 px-10 py-32 mx-auto bg-white lg:flex-row">
+			<div className="flex-1 space-y-6">
+				<Badge className="mb-2">About Us</Badge>
+				<h1 className="text-4xl font-semibold">
+					Kenali Lebih Dekat SGA{" "}
+					<span className="text-blue-500">Cakrawala University</span>
+				</h1>
+				<p className="text-gray-300">
+					Student Government Association (SGA) Cakrawala University adalah wadah
+					resmi bagi mahasiswa untuk berpartisipasi dalam pengambilan keputusan
+					yang mempengaruhi kehidupan kampus. Kami berkomitmen untuk menjadi
+					jembatan antara mahasiswa dan pihak universitas, serta mempromosikan
+					kepemimpinan yang inklusif.
+				</p>
+			</div>
+			<img src={sgaMembers} />
+		</section>
+	);
 }
