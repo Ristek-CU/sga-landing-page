8628eb0f444067972266ba94c97ee4bb8e41b04f
RugeFX <zackfxg@gmail.com>
2025-02-03 00:56:57 +0700

    fix(hero): put particles behind main elements

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
 	return (
 		<section className="relative flex flex-col w-full min-h-screen overflow-hidden text-white bg-hero bg-hero-pattern">
 			<Header />
-			<div className="flex flex-col items-center justify-center w-full h-full gap-4 min-h-[768px] container mx-auto px-10">
+			<div className="flex flex-col items-center justify-center w-full h-full gap-4 min-h-[768px] container mx-auto px-10 z-[1]">
 				<div className="group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-full bg-white/10 px-5 py-1 text-sm font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-xs transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f]">
 					<div className="absolute inset-0 block h-full w-full animate-gradient bg-linear-to-r from-blue-300/50 via-green-300/100 to-yellow-300/100 bg-[length:var(--bg-size)_100%] p-[1px] [mask-composite:subtract]! [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]" />
 					<span className="inline-block text-lg font-semibold text-center text-transparent text-max bg-linear-to-r from-blue-300 via-green-300 to-yellow-300 bg-clip-text">
