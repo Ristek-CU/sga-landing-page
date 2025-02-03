ec18ed602d71fe3c7d2fdb24f3a5bdde52efc06a
RugeFX <zackfxg@gmail.com>
2025-02-04 01:01:08 +0700

    feat(responsive): applied responsive styles for vision section

--- src/components/sections/vision/index.tsx
+++ src/components/sections/vision/index.tsx
@@ -3,15 +3,15 @@ import { Badge } from "@/components/ui/badge";
 
 export default function VisionSection() {
 	return (
-		<section className="flex flex-col w-full h-full container gap-10 px-10 py-32 mx-auto bg-white lg:flex-row">
+		<section className="container flex flex-col-reverse items-center w-full h-full px-10 py-32 mx-auto bg-white gap-x-10 gap-y-5 lg:flex-row">
 			<img src={sgaMembers} className="flex-1 w-full h-max" />
-			<div className="flex-1 space-y-6">
+			<div className="flex-1 space-y-5">
 				<Badge>Vision</Badge>
-				<h1 className="text-4xl font-semibold leading-tight">
+				<h1 className="text-2xl font-semibold leading-tight xl:text-4xl">
 					Inspirasi Kepemimpinan dan Inovasi di{" "}
 					<span className="text-green-500">Lingkungan Kampus</span>
 				</h1>
-				<p className="text-xl font-normal text-gray-300">
+				<p className="text-sm font-normal text-gray-300 xl:text-xl xl:mt-10">
 					Menjadi pelopor dalam pengembangan kepemimpinan mahasiswa yang
 					berkelanjutan, dengan menciptakan lingkungan yang mendorong inovasi,
 					kolaborasi, dan inklusivitas. Kami berkomitmen untuk memberdayakan
