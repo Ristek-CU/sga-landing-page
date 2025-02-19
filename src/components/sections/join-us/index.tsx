47d17eec94bc3de6fd11f79ef552a7e53caabc8c
RugeFX <zackfxg@gmail.com>
2025-02-20 00:09:09 +0700

    chore(seo): removed unused import

--- src/components/sections/join-us/index.tsx
+++ src/components/sections/join-us/index.tsx
@@ -1,5 +1,5 @@
 import { Badge } from "@/components/ui/badge";
-import Button, { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Marquee } from "@/components/ui/marquee";
 import Particles from "@/components/ui/particles";
 
